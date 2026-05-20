01d29d7c23bcc54dff9cccd392bb31922e511356
Marco Vasquez <marco.vasquez.falcon@udla.edu.ec>
2026-05-20 15:12:59 -0500

    Cambio de funcion por punteros final

--- Desktop/Segundo semestre Software/Programacion/VasquezMarco205.c
+++ Desktop/Segundo semestre Software/Programacion/VasquezMarco205.c
@@ -1,19 +1,25 @@
-
 #include <stdio.h>
-float calcular_promedio(float notas[], int tamaño);
+
+float calcular_promedio(float *ptr, int tamano);
+
 int main() {
-    float notas[]={6.5, 7.1, 8, 6, 5};
-    float promedio=calcular_promedio(notas,5);
-    printf("\nel promedio de las notas %.1f\n", promedio);
+    float notas[] = {6.5, 7.1, 8.0, 6.0, 5.0};
+    
+    float promedio = calcular_promedio(notas, 5);
+    
+    printf("\nEl promedio de las notas es: %.1f\n", promedio);
     return 0;
 }
-float calcular_promedio(float notas[], int tamaño){
-    float suma=0;
-    float promedio=0;
-    for (int i=0; i<tamaño; i++){
-        printf("nota %d: %.1f\n",i+1, notas[i]);
-        suma = suma+notas[i];
+
+float calcular_promedio(float *ptr, int tamano) {
+    float suma = 0;
+    float promedio = 0;
+    
+    for (int i = 0; i < tamano; i++) {
+        printf("Nota %d: %.1f\n", i + 1, ptr[i]);
+        suma += ptr[i];
     }
-    promedio=suma/tamaño;
+    
+    promedio = suma / tamano;
     return promedio;
 }
