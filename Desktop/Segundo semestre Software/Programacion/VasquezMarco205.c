f42a139775524868ef45f133231fcff45e1502c6
Marco Vasquez <marco.vasquez.falcon@udla.edu.ec>
2026-05-20 15:07:12 -0500

    funcion promedio

--- Desktop/Segundo semestre Software/Programacion/VasquezMarco205.c
+++ Desktop/Segundo semestre Software/Programacion/VasquezMarco205.c
@@ -1,18 +1,19 @@
+
 #include <stdio.h>
-
+float calcular_promedio(float notas[], int tamaño);
 int main() {
-    float notas[] = {6.5, 7.1, 8, 6, 5};
-    float suma = 0.0;
-    float promedio;
-
-    for (int i = 0; i < 5; i++) {
-        printf("nota %d: %.1f\n", i + 1, notas[i]);
-        suma += notas[i];
-    }
-
-    promedio = suma / 5;
-
-    printf("\nEl promedio de las notas es: %.2f\n", promedio);
-
+    float notas[]={6.5, 7.1, 8, 6, 5};
+    float promedio=calcular_promedio(notas,5);
+    printf("\nel promedio de las notas %.1f\n", promedio);
     return 0;
+}
+float calcular_promedio(float notas[], int tamaño){
+    float suma=0;
+    float promedio=0;
+    for (int i=0; i<tamaño; i++){
+        printf("nota %d: %.1f\n",i+1, notas[i]);
+        suma = suma+notas[i];
+    }
+    promedio=suma/tamaño;
+    return promedio;
 }
