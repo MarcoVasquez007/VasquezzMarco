03e0abf0ba6c63d42a6120bf6ab2bf685cd3dcaf
Marco Vasquez <marco.vasquez.falcon@udla.edu.ec>
2026-05-20 14:38:34 -0500

    Promedio

--- Desktop/Segundo semestre Software/Programacion/VasquezMarco205.c
+++ Desktop/Segundo semestre Software/Programacion/VasquezMarco205.c
@@ -1,18 +1,18 @@
-
-
 #include <stdio.h>
- 
+
 int main() {
+    float notas[] = {6.5, 7.1, 8, 6, 5};
+    float suma = 0.0;
+    float promedio;
 
-    float notas[]={6.5, 7.1, 8, 6, 5};
-
-    for (int i=0; i<5; i++){
-
-        printf("nota %d: %.1f\n",i+1, notas[i]);
-
+    for (int i = 0; i < 5; i++) {
+        printf("nota %d: %.1f\n", i + 1, notas[i]);
+        suma += notas[i];
     }
 
-    return 0;
+    promedio = suma / 5;
 
+    printf("\nEl promedio de las notas es: %.2f\n", promedio);
+
+    return 0;
 }
- 
